eda8c2d8f8ea4a07855d082f0315d1968053ed55
dharaluxe <krishhtri7@gmail.com>
2025-04-10 13:37:13 +0530

    Add files via upload

---
+++ dhara_luxe_site/src/HomePage.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: '2rem', textAlign: 'center' }}>
+      <h1>Dhara Luxe Homepage</h1>
+      <p>Replace this with your full HomePage.js code.</p>
+    </div>
+  );
+}
